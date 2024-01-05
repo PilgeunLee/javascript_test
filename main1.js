27f628e1312d4efd7ad64f2cb7063c542ad69b43
PilgeunLee <pgstom9015@gmail.com>
2024-01-05 21:50:06 +0900

    'axios수정'

--- main1.js
+++ main1.js
@@ -2,13 +2,13 @@ import axios from 'axios'
 
 function fetchMovies(){
     axios
-    .get('https://www.omdbapi.com.?apikey=7060c60c&s=frozen')
+    .get('https://www.omdbapi.com?apikey=7035c60c&s=frozen')
     .then(res =>{
         console.log(res)
-        const h1El =document.querySelector('h1')
+        const h1El = document.querySelector('h1')
         const imgEl = document.querySelector('img')
         h1El.textContent = res.data.Search[0].Title
-        imgEl.textContent = res.data.Search[0].Poster
+        imgEl.src = res.data.Search[0].Poster
     })
 
 }
